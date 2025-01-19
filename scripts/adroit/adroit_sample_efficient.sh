8d58d30bb279fa3a201b72de355e7c31bcef5f89
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-19 16:45:41 +0000

    Add missing library paths to environment variables
    Ensure the script has access to Mujoco and NVIDIA libraries by updating the `LD_LIBRARY_PATH`. This change resolves potential runtime issues caused by missing dependencies.

--- scripts/adroit/adroit_sample_efficient.sh
+++ scripts/adroit/adroit_sample_efficient.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bentengma/.mujoco/mujoco210/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+
 seeds=(1014 2937 5382 4785 7913)
 demos=5
 name_prefix="rfcl_sample_efficient"
